fd303dbb49361436d02a09f161d7728d9a3b66a3
daleonpz <daleonpz@gmail.com>
2017-05-16 12:26:34 -0500

    update

--- freshinstall/afterinstall.sh
+++ freshinstall/afterinstall.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
 ## set dvorak layout
-sudo loadkeys dvorak-programmer
+loadkeys dvorak-programmer
+
+
+## Pacman Tips
+## https://wiki.manjaro.org/index.php?title=Pacman_Tips
+# Ranking mirrors
+pacman-mirrors -g
+pacman -Syy
+
+# Fasttrack your mirrorlist
+pacman-mirrors -f 5
+pacman -Syy
+
+# Optimize the database access speed
+pacman-optimize && sync
+
 
 ## basic programs
-sudo pacman -S octopi
-sudo pacman -S valgrind
-yes | sudo pacman -S vim vifm firejail zathura-pdf-mupdf zathura-djvu
+pacman -S octopi
+pacman -S valgrind
+pacman -S tilda
+pacman -S wget  yaourt
+pacman -S vi vim vifm firejail zathura-pdf-mupdf zathura-djvu
+
+# Packer
+# Uses some of the same commands as pacman but differs in that it checks both the official repos & AUR.
+yaourt -S packer
+packer -S gcal
+
+
 
 ## git clones
 mkdir ~/Documents/gitStuff
@@ -16,11 +40,12 @@ git clone git@github.com:daleonpz/dnl_tools.git
 git clone git@github.com:daleonpz/blog.git
 cd ~
 
-
 ## move dotfiles 
 cd ~/Documents/gitStuff/dotfiles
 sh restore_dotfiles.sh
 cd ~
+
+
 
 ## in firefox
 # vimperator
